c03dcbc4c9c0fdddb7137de93a7a30ba8f3ab69b
potatopc <contact@locgfx.com>
2023-08-29 11:16:50 +0530

    29 August saurabh main

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -1,3 +1,3 @@
-const String baseUrl = 'https://swishlist.locgfx.com';
+const String baseUrl = 'https://api.swishlist.com';
 const String baseUrl2 = 'https://swishlist.locgfx.com/';
 const String etsyUrl = 'https://openapi.etsy.com';
